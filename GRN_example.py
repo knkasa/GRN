4a83d93e960abbfc2217e10079ff0e4595836606
Ken Nakatsukasa <knkasa@gmail.com>
2025-06-07 09:54:34 +0900

    Update GRN_example.py

--- GRN_example.py
+++ GRN_example.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 from tensorflow.keras import layers
 
-# Gated Residual Network (work for table)
+# Gated Residual Network (work for tabular data)
 # https://medium.com/chat-gpt-now-writes-all-my-articles/gated-residual-networks-a-modern-secret-weapon-for-tabular-deep-learning-7a8d247a01d1
 
 
